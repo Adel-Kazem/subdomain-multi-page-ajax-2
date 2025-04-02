c71abb911355c8b2674e83f23bea59709602b0f4
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-02 18:06:02 +0300

    Update products via frontend

--- products.js
+++ products.js
@@ -76,9 +76,9 @@ const PRODUCTS = [
   },
   {
     id: 2,
-    sku: "",
+    sku: "asdf",
     name: 'Smart Fitness Tracker',
-    slug: '',
+    slug: 'asdf',
     description: 'Track your health and fitness goals with our advanced smart fitness tracker. Monitor heart rate, sleep patterns, and activity levels with precision sensors and a vibrant display.',
     metaDescription: '',
     price: 89.99,
@@ -109,9 +109,7 @@ const PRODUCTS = [
       freeShipping: false
     },
     images: [
-      'https://images.unsplash.com/photo-1575311373937-040b8e1fd6b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
-      'https://images.unsplash.com/photo-1551816230-ef5deaed4a26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80',
-      'https://images.unsplash.com/photo-1626371353531-b2cf4f8a4038?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+      'https://images.unsplash.com/photo-1551816230-ef5deaed4a26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
     ],
     variants: [],
     features: [
