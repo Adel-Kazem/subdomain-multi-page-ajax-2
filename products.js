10ce05f20998b4db84cd3b0fb73115958a98da18
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-10 20:33:35 +0300

    Update products via frontend

--- products.js
+++ products.js
@@ -616,5 +616,49 @@ const PRODUCTS = [
  categories: [],
  createdAt: "2025-04-10T17:31:28.198Z",
  updatedAt: "2025-04-10T17:31:28.198Z"
+ },
+ {
+ id: 3007,
+ name: "asdfasdf",
+ description: "",
+ base_price: 0,
+ base_shipping_cost: 0,
+ free_shipping: false,
+ sku: "PRD-3007",
+ slug: "new-product",
+ brand: "",
+ rating: 0,
+ reviews_count: 0,
+ status: "active",
+ isFeatured: false,
+ isOnSale: false,
+ requiresShipping: true,
+ requiresInventoryTracking: true,
+ stock: 0,
+ hasVariants: false,
+ totalVariantStock: 0,
+ lowStockThreshold: 5,
+ features: {},
+ images: [],
+ options: {},
+ option_images: {},
+ variant_images: {},
+ option_variants_stock: {},
+ option_price_adjustments: {},
+ option_shipping_adjustments: {},
+ weight: {
+ value: 0,
+ unit: "kg"
+ },
+ dimensions: {
+ length: 0,
+ width: 0,
+ height: 0,
+ unit: "cm"
+ },
+ option_dimension_overrides: {},
+ categories: [],
+ createdAt: "2025-04-10T17:33:27.246Z",
+ updatedAt: "2025-04-10T17:33:27.246Z"
  }
 ];
