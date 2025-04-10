7a346f7acef11a279641e3d12e7b5a38224cc106
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-10 20:12:52 +0300

    Update products via frontend

--- products.js
+++ products.js
@@ -394,49 +394,5 @@ const PRODUCTS = [
  ],
  createdAt: "2025-01-05T09:00:00Z",
  updatedAt: "2025-04-10T17:00:40.095Z"
- },
- {
- id: 3002,
- name: "New Product",
- description: "",
- base_price: 0,
- base_shipping_cost: 0,
- free_shipping: false,
- sku: "PRD-3002",
- slug: "new-product",
- brand: "",
- rating: 0,
- reviews_count: 0,
- status: "active",
- isFeatured: false,
- isOnSale: false,
- requiresShipping: true,
- requiresInventoryTracking: true,
- stock: 0,
- hasVariants: false,
- totalVariantStock: 0,
- lowStockThreshold: 5,
- features: {},
- images: [],
- options: {},
- option_images: {},
- variant_images: {},
- option_variants_stock: {},
- option_price_adjustments: {},
- option_shipping_adjustments: {},
- weight: {
- value: 0,
- unit: "kg"
- },
- dimensions: {
- length: 0,
- width: 0,
- height: 0,
- unit: "cm"
- },
- option_dimension_overrides: {},
- categories: [],
- createdAt: "2025-04-10T17:12:30.473Z",
- updatedAt: "2025-04-10T17:12:30.473Z"
  }
 ];
