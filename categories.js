acd074e7c84c910fc27b44d14dcbdba34313ce5c
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-13 11:33:54 +0300

    Update category product assignments

--- categories.js
+++ categories.js
@@ -11,8 +11,7 @@ const CATEGORIES = [
  imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
  parent_id: null,
  productIds: [
- 3001,
- 3002
+ 3001
  ],
  updated_at: "2025-04-12T15:27:50.077Z"
  },
@@ -28,8 +27,7 @@ const CATEGORIES = [
  imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
  parent_id: 1,
  productIds: [
- 3001,
- 3002
+ 3001
  ],
  updated_at: "2025-04-12T19:13:28.986Z"
  }
