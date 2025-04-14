e6967000bfc53a9db7792d7df226c7d88a8109b9
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-14 14:29:14 +0300

    Update products via frontend

--- products.js
+++ products.js
@@ -433,7 +433,7 @@ const PRODUCTS = [
  3003
  ],
  createdAt: "2025-01-05T09:00:00Z",
- updatedAt: "2025-04-14T11:17:35.017Z",
+ updatedAt: "2025-04-14T11:29:06.691Z",
  _categoryChanges: {
  added: [],
  removed: []
