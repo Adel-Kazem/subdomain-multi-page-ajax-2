afa7dc08c749bcb9a9f9edd836ca2226c4b265ad
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-14 17:08:21 +0300

    Bulk update: Edited 2 categories

--- categories.js
+++ categories.js
@@ -2,33 +2,29 @@ const CATEGORIES = [
  {
  id: 1,
  name: "Modi perspiciatis e",
- description: "Laborum Voluptatibu",
  slug: "Ad consequuntur veli",
- active: true,
- created_at: "2025-04-12T15:27:50.077Z",
- display_order: 76,
- featured: true,
+ description: "Laborum Voluptatibu",
  imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
+ featured: true,
  parent_id: null,
- productIds: [
- 3001
- ],
- updated_at: "2025-04-12T15:27:50.077Z"
+ active: true,
+ display_order: 76,
+ productIds: [],
+ created_at: "2025-04-12T15:27:50.077Z",
+ updated_at: "2025-04-14T14:08:18.888Z"
  },
  {
  id: 2,
  name: "test 2",
- description: "Et nisi voluptas mol",
  slug: "Cupidatat officia re",
- active: false,
- created_at: "2025-04-12T19:13:28.986Z",
- display_order: 35,
- featured: true,
+ description: "Et nisi voluptas mol",
  imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
+ featured: true,
  parent_id: 1,
- productIds: [
- 3001
- ],
- updated_at: "2025-04-12T19:13:28.986Z"
+ active: false,
+ display_order: 35,
+ productIds: [],
+ created_at: "2025-04-12T19:13:28.986Z",
+ updated_at: "2025-04-14T14:08:18.888Z"
  }
 ];
