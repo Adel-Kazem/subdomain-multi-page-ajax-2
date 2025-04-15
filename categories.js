84db54921439a3b2b460712a5eeb23560d14d43d
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-04-15 14:08:03 +0300

    Bulk update: edited 1 categories

--- categories.js
+++ categories.js
@@ -1,88 +1,91 @@
 const CATEGORIES = [
- {
- id: 1,
- name: "Modi perspiciatis e",
- slug: "Ad consequuntur veli",
- description: "Laborum Voluptatibu",
- imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
- featured: true,
- parent_id: null,
- active: true,
- display_order: 76,
- productIds: [],
- created_at: "2025-04-12T15:27:50.077Z",
- updated_at: "2025-04-14T18:46:56.038Z"
- },
- {
- id: 2,
- name: "test 2",
- slug: "Cupidatat officia re",
- description: "Et nisi voluptas mol",
- imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
- featured: true,
- parent_id: 1,
- active: false,
- display_order: 35,
- productIds: [],
- created_at: "2025-04-12T19:13:28.986Z",
- updated_at: "2025-04-14T14:08:18.888Z"
- },
- {
- id: 1000,
- name: "test 2 sadf",
- slug: "new-category",
- description: "test 2 test 2",
- imageUrl: "",
- featured: false,
- parent_id: null,
- active: false,
- display_order: 12,
- productIds: [
- 3002
- ],
- created_at: "2025-04-14T14:13:45.390Z",
- updated_at: "2025-04-15T07:40:27.449Z"
- },
- {
- id: 1001,
- name: "New Category ",
- slug: "new-category",
- description: "new category!",
- imageUrl: "",
- featured: false,
- parent_id: null,
- active: true,
- display_order: 13,
- productIds: [],
- created_at: "2025-04-15T07:16:54.827Z",
- updated_at: "2025-04-15T07:16:54.827Z"
- },
- {
- id: 1002,
- name: "New Category33 ",
- slug: "new-category",
- description: "3333",
- imageUrl: "",
- featured: false,
- parent_id: null,
- active: true,
- display_order: 14,
- productIds: [],
- created_at: "2025-04-15T07:36:01.883Z",
- updated_at: "2025-04-15T07:36:01.883Z"
- },
- {
- id: 1003,
- name: "New Category44",
- slug: "new-category",
- description: "44",
- imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/377903_mrp_fr_l.jpg",
- featured: false,
- parent_id: null,
- active: true,
- display_order: 15,
- productIds: [],
- created_at: "2025-04-15T09:03:01.478Z",
- updated_at: "2025-04-15T09:03:01.478Z"
- }
+  {
+    id: 1,
+    name: "Modi perspiciatis e",
+    slug: "Ad consequuntur veli",
+    description: "Laborum Voluptatibu",
+    imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
+    featured: true,
+    parent_id: null,
+    active: true,
+    display_order: 76,
+    productIds: [],
+    created_at: "2025-04-12T15:27:50.077Z",
+    updated_at: "2025-04-14T18:46:56.038Z"
+  },
+  {
+    id: 2,
+    name: "test 2",
+    slug: "Cupidatat officia re",
+    description: "Et nisi voluptas mol",
+    imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/perfect images.png",
+    featured: true,
+    parent_id: 1,
+    active: false,
+    display_order: 35,
+    productIds: [],
+    created_at: "2025-04-12T19:13:28.986Z",
+    updated_at: "2025-04-14T14:08:18.888Z"
+  },
+  {
+    id: 1000,
+    name: "test 2 sadf",
+    slug: "new-category",
+    description: "test 2 test 2",
+    imageUrl: "",
+    featured: false,
+    parent_id: null,
+    active: false,
+    display_order: 12,
+    productIds: [
+      3002
+    ],
+    created_at: "2025-04-14T14:13:45.390Z",
+    updated_at: "2025-04-15T07:40:27.449Z"
+  },
+  {
+    id: 1001,
+    name: "New Category ",
+    slug: "new-category",
+    description: "new category!",
+    imageUrl: "",
+    featured: false,
+    parent_id: null,
+    active: true,
+    display_order: 13,
+    productIds: [],
+    created_at: "2025-04-15T07:16:54.827Z",
+    updated_at: "2025-04-15T07:16:54.827Z"
+  },
+  {
+    id: 1002,
+    name: "New Category33 ",
+    slug: "new-category",
+    description: "3333",
+    imageUrl: "",
+    featured: false,
+    parent_id: null,
+    active: true,
+    display_order: 14,
+    productIds: [],
+    created_at: "2025-04-15T07:36:01.883Z",
+    updated_at: "2025-04-15T07:36:01.883Z"
+  },
+  {
+    id: 1003,
+    name: "New Category44",
+    slug: "new-category",
+    description: "44",
+    imageUrl: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-multi-page-ajax-2/main/category_images/377903_mrp_fr_l.jpg",
+    featured: false,
+    parent_id: null,
+    active: true,
+    display_order: 15,
+    productIds: [
+      1001,
+      2001
+    ],
+    created_at: "2025-04-15T09:03:01.478Z",
+    updated_at: "2025-04-15T11:07:56.694Z"
+  }
 ];
